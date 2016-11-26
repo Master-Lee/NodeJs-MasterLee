b497c7d8233067718c7945e8092708f894ea3be7
Master Lee <549517621@qq.com>
2016-11-26 23:18:55 +0800

    用匿名函数保留全局变量的值

---
+++ 闭包/funcSaveGlobalValue.js
@@ -0,0 +1,22 @@
+//用匿名函数保留全局变量的值 express实战 p53
+function asyncFunction(callback){
+	setTimeout(callback, 200);
+}
+
+function asyncFunction2(callback){
+	setInterval(callback,200);
+}
+
+var color = 'blue';
+
+(function(color){
+	asyncFunction(function(){
+		console.log('The color is ' + color);
+	});
+
+	asyncFunction2(function(){
+		console.log('The color is ' + color);
+	});
+})(color);
+
+color = 'green';
