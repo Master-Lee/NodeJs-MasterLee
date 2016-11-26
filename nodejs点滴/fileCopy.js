82f58ff309248a04bf4aa1c83ffe472380d23f4d
Master Lee <549517621@qq.com>
2016-11-26 21:37:29 +0800

    端口扫描

---
+++ nodejs点滴/fileCopy.js
@@ -0,0 +1,57 @@
+ var fs = require('fs');
+ var fdPath = require('path');
+ stat = fs.stat;
+
+ var copy = function(src, dst){
+ 	//读取目录中的所有文件/目录
+ 	fs.readdir(src,function(err, paths){
+ 		if(err){
+ 			throw err;
+ 		}
+ 		console.log(paths);
+ 		paths.forEach(function(path){
+ 			var _src = fdPath.join(src,path),
+ 				_dst = fdPath.join(dst,path),
+ 				readable, writeable;
+ 			console.log(_src);
+ 			console.log(_dst);
+ 			stat(_src,function(err,st){
+ 				if(err){
+ 					throw err;
+ 				}
+
+ 				//判断是否为文件
+ 				if(st.isFile()){
+ 					//创建读取流
+ 					readable = fs.createReadStream(_src);
+ 					//创建写入流
+ 					writeable = fs.createWriteStream(_dst);
+
+ 					//通过管道来传输流
+ 					readable.pipe(writeable);
+ 				}
+ 				else if(st.isDirectory()){
+ 					//如果是目录，则递归
+ 					exists(_src,_dst,copy);
+ 				}
+ 			});
+ 		});
+ 	});
+ };
+
+ var exists = function(src,dst,callback){
+ 	fs.exists(dst,function(exists){
+ 		if(exists)
+ 		{
+ 			callback(src,dst);
+ 		}
+ 		else
+ 		{
+ 			fs.mkdir(dst,function(){
+ 				callback(src,dst);
+ 			});
+ 		}
+ 	});
+ };
+
+ exists('./src','./dst2',copy);
---
+++ nodejs点滴/portScan.js
@@ -0,0 +1,37 @@
+var net = require('net');
+var result = [];
+
+function scan(host, start, end, callback){
+	console.time('port scan time');
+	var count = end - start;
+	for(var i = start; i <= end; i++){
+		var item = net.connect({host:host,
+								port:i},
+								function(i){
+									return function(){
+										result.push(i);
+										this.destroy();
+									}
+								}(i));
+		item.on('error',function(err){
+			if(err.errno === 'ECONNREFUSED')
+			{
+				this.destroy();
+			}
+		});
+
+		item.on('close', function(){
+			if(!count--){
+				console.timeEnd('port scan time');
+				callback(result);
+			}
+		});
+	}
+};
+
+scan('192.168.1.1', 1,200,function(result){
+	for(var i = 0; i < result.length; i++)
+	{
+		console.log('port：' + result[i] + ' is open!');
+	}
+});
